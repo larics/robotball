0d45881d48a8fb6f18ad3c370da578749e851c3a
Marko Krizmancic <marko.krizmancic@fer.hr>
2021-04-27 17:01:01 +0200

    Organize and fix arduino code

---
+++ robotball_arduino/main/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <math.h>
+#include "utilities.h"
+#include "ros_stuff.h"
+#include "motor.h"
+
+// Geometry configuration
+const float g_ws = 0.323;		// Wheel separation (m) == 32.3 cm
+const float g_lwr = 0.03875; 	// Left wheel radius (m) == 38.75 mm
+const float g_rwr = 0.03875; 	// Right wheel radius (m) == 38.75 mm
+
+#endif
---
+++ robotball_arduino/main/utilities.h
@@ -0,0 +1,7 @@
+#ifndef _UTILS_H
+#define _UTILS_H
+
+const float rpm2rad = 0.104719755;
+const float rad2rpm = 9.549296596;
+
+#endif
