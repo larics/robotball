d63dad1ab948bcdddd394e2b4e8dfae5c2d1c714
Marko Krizmancic <marko.krizmancic@fer.hr>
2021-11-05 15:42:58 +0100

    Clean up the repository

--- robotball_arduino/rb_joystick_control/my_ahrs.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MY_AHRS_H_
-#define _MY_AHRS_H_
-
-bool AHRS_setup();
-void AHRS_update(double*, double*, double*);
-
-#endif
--- robotball_arduino/readSerial.py
+++
@@ -1,5 +0,0 @@
-import serial
-
-arduino = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-while True:
-	print(arduino.readline())
